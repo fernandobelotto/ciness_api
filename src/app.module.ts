bcb9219eacff16a2f568dd44942e8de5f96faef5
fernandobosco <fbosco@dlocal.com>
2022-01-22 23:57:30 -0300

    update with postgres and swagger

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AppController } from './controllers/app.controller';
+import { AppService } from './services/app.service';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { User } from './models/users.model';
 
 @Module({
-  imports: [],
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'mysecretpassword',
+      database: 'test',
+      models: [User],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
+
+//docker run --name some-postgres -p 5432:5432 -e POSTGRES_PASSWORD=mysecretpassword -d postgres
+// docker exec -it -u postgres some-postgres psql
